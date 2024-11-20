47739a3373b3ff266d949ae9f9decf62bec85bcf
Shivam-Kharbanda <shivamk290705@gmail.com>
2024-11-20 17:51:28 +0530

    Maximum area

---
+++ maximum-area.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+void solve(int *arr, int n) {
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int area = ((j - i) * arr[i] + (0.5 * (j - i) * (arr[j] - arr[i])));
+            maxi = max(area, maxi);
+        }
+    }
+
+    cout << "Maximum area is: " << maxi << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 9, 10}; // Array must be sorted
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    solve(arr, n);
+
+    return 0;
+}
